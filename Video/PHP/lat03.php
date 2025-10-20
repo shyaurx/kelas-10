f52d3bde59bd563bffd091faad03c51137337b2c
shyaurx <shifaauranuradiansyah@gmail.com>
2025-10-20 09:15:46 +0700

    Add files via upload

---
+++ Video/PHP/lat03.php
@@ -0,0 +1,25 @@
+<?php
+
+    function belajar(){
+        echo "saya belajar PHP";
+    }
+
+    function luasPersegi($p = 5, $l = 3){
+        $luas = $p * $l;
+
+        echo $luas;
+    }
+
+    function luas($p = 5, $l = 3){
+        $luas = $p * $l;
+
+        return $luas;
+    }
+
+    function output(){
+        return "belajar function";
+    }
+
+    echo luas(100,3) * 5;
+
+?>
