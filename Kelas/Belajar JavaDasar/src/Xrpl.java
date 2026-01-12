2be52324fea0068c8498baa345a2d8cc3dabaf2e
shyaurx <shifaauranuradiansyah@gmail.com>
2026-01-12 09:39:09 +0700

    Add files via upload

---
+++ Kelas/Belajar JavaDasar/src/Xrpl.java
@@ -0,0 +1,33 @@
+
+public class Xrpl {
+        
+    
+   public static void main(String[] args)  {
+       int Angka;
+       Angka=10;
+       
+            System.out.println(Angka);
+            System.out.println("Angka yang anda inputkan: "+Angka);
+       Angka=100;
+            System.out.println("Angka terbaru adalah: "+Angka);
+            
+       double desimal=10.355;
+       float pecahan= 13.4555f;
+            System.out.println(desimal);
+            System.out.println("Angka pecahan yang diimput: "+pecahan);
+            
+       String nama= "Shifa";
+       char abjad= 'A';   
+            System.out.println(nama);
+            System.out.println(abjad);
+            
+       boolean betul = true;
+       boolean salah = false;
+            System.out.println(betul);
+            System.out.println(salah);
+            
+       //desimal= 100.235;
+            System.out.println(desimal);
+   }
+    
+}
